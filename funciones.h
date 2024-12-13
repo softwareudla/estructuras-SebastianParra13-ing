692fc42399b6df95c682f5765a95df7158a7524c
SebastianParra13-ing <sebasparra676@gmail.com>
2024-12-13 10:22:22 -0500

    enviar

---
+++ funciones.h
@@ -0,0 +1,19 @@
+#define maxlib 20
+
+struct Libro
+{
+    int id;
+    char titulo[100];
+    char Autor[50];
+    int anio;
+    char Estado[10];
+};
+struct Libro libros[maxlib];
+int menu();
+void actulizarEstad(struct Libro libro[maxlib], int *indice);
+void registrarLibros(struct Libro libro[maxlib], int *indice);
+void mostrarLibros(struct Libro libro[maxlib], int *n);
+void buscarLibroID(struct Libro libro[maxlib], int *indice);
+void buscarLibroTi(struct Libro libro[maxlib], int *indice);
+void buscarLibro(struct Libro libro[maxlib],int *indice);
+void eliminarLib(struct Libro libro[maxlib], int *indice);
